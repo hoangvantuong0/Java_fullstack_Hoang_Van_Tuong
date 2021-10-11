9584d1febf7d0400104a0b413ce079feec60fffa
hoangvantuong0 <hoangvantuongdhqt@gmail.com>
2021-10-11 21:23:12 +0700

    up bai

---
+++ module_3/bai_01/bai_tap/bai_1.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE mydatabase;
+USE mydatabase;
+CREATE TABLE student(
+id INT NOT NULL,
+name VARCHAR(45) NULL,
+age INT NULL,
+country VARCHAR(45) NULL,
+PRIMARY KEY(id));
+CREATE TABLE class(
+id INT NOT NULL,
+name VARCHAR(45) NULL,
+PRIMARY KEY(id));
+CREATE TABLE teacher(
+id INT NOT NULL,
+name VARCHAR(45) NULL,
+age INT NULL,
+country VARCHAR(45) NULL,
+PRIMARY KEY(id));
---
+++ module_3/bai_01/thuc_hanh/bai_1.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE mydatabase;
+USE mydatabase;
+CREATE TABLE student(
+id INT NOT NULL,
+name VARCHAR(45) NULL,
+age INT NULL,
+country VARCHAR(45) NULL,
+PRIMARY KEY(id));
